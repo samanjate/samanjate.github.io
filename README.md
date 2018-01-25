9969fd259eefd1b2ec1d2c3234c684ac209377ee
Samanjate Sood <samanjate@gmail.com>
2018-01-24 23:07:14 -0500

    modified template to personalize site

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ This is the personal website of Samanjate Sood
 ## Acknowledgments
 
 * [Parallax theme](https://www.w3schools.com/w3css/tryw3css_templates_parallax.htm) from w3schools.
+* [Typed.js](https://mattboldt.com/demos/typed-js/)
---
+++ js/custom.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function () {
+  Typed.new('.typewriter', {
+    strings: ["Samanjate Sood.",
+              "a CS grad student at NEU.",
+              "majoring in Software Engineering.",
+              "also studing Database Management and Artifical Intelligence.",
+              "a pretty good photographer too!"],
+    startDelay: 2000,
+    typeSpeed: 15,
+    backSpeed: 0,
+    loop: true
+  })
+});
+
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
